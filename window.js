fd764445a7cbdc7bf2c5da5aad0b3fc9ded2ed74
Danyl Fernandes <androidan1304@gmail.com>
2021-06-21 06:51:29 -0700

    Refactor functions

--- window.js
+++ window.js
@@ -116,18 +116,12 @@ function fetchBibleVerse (bibleSearch) {
                 hideLoading()
                 console.log('Please check the book and reference you specified.');
             } else {
-                
-
                 let ref = full_ref.substring(0, full_ref.lastIndexOf(' ')) + " " + bibleSearch[3].toUpperCase();
 
                 console.log(`Verse => ${verse} -- ${ref}`);
 
-                clipboardy.writeSync(`${verse}\n${ref}`);
-                notifier.notify({
-                    title: 'Bleezy',
-                    message: `${ref} was copied to the clipboard`
-                });  
-
+                copyBibleVerseToClipboard(verse, ref);
+                notifyUser(ref)
                 displayVerse(verse, ref);
                 clearInputFieldsAndFocusFirstField();
             }
@@ -142,76 +136,15 @@ function getConstructedUrlFromBibleSearchArray(bibleSearch) {
             +${bibleSearch[1]}%3A${bibleSearch[2]}&version=${bibleSearch[3]}`;
 }
 
-function fetchBibleVerses(bibleSearch) {
-    const request = require('request');
-    const cheerio = require('cheerio');
-    const clipboardy = require('clipboardy');
-    const notifier = require('node-notifier');
+function copyBibleVerseToClipboard(verse, ref) {
+    clipboardy.writeSync(`${verse}\n${ref}`);
+}
 
-    let messageDialog = $('#message')
-
-    let trimmedInputString = inputString.trim()
-
-    let firstIndexOfSpace = trimmedInputString.indexOf(" ") + 1
-    let lastIndexOfSpace = trimmedInputString.lastIndexOf(" ") + 1
-
-    let b_book = trimmedInputString.substring(0, firstIndexOfSpace)
-    let b_reference = trimmedInputString.substring(firstIndexOfSpace, lastIndexOfSpace)
-    let b_translation = trimmedInputString.substring(lastIndexOfSpace, trimmedInputString.length)
-
-    if(b_reference == '') {
-        b_reference = b_translation
-        b_translation = 'nkjv'
-    } 
-
-    console.log(`Book: ${b_book}\nRef: ${b_reference}\nTranslation: ${b_translation}`)
-    
-    let b_chapter, b_verse, delimiter;
-    
-    if (b_reference.includes(':')) {
-        delimiter = ':';
-    } else if (b_reference.includes('.')) {
-        delimiter = '.';
-    } else if (b_reference.includes(',')) {
-        delimiter = ',';
-    } else {
-        console.log(`Error: Please use ':' or '.' or ',' to seperate the chapter and verse numbers`);
-    }
-
-    b_chapter = b_reference.substring(0, b_reference.indexOf(delimiter));
-    b_verse = b_reference.substring(b_reference.indexOf(delimiter) + 1, b_reference.length);
-
-    const url = `https://www.biblegateway.com/passage/?search=${b_book}+${b_chapter}%3A${b_verse}&version=${b_translation}`;
-
-    showLoading()
-    request(url, (error, response, html) => {
-        if (!error && response.statusCode == 200) {
-            const $ = cheerio.load(html);
-            let verse = $('meta[property="og:description"]').attr('content');
-            let full_ref = $('meta[name="twitter:title"]').attr('content');
-            
-            console.log(`\nURL => ${url}`);
-
-            if (typeof verse === 'undefined' || typeof full_ref === 'undefined') {
-                hideLoading()
-                $('#message').text('Please check the book and reference you specified.');
-            } else {
-                let ref = full_ref.substring(0, full_ref.lastIndexOf(' ')) + " " + b_translation.toUpperCase();
-
-                console.log(`Verse => ${verse} -- ${ref}`);
-
-                clipboardy.writeSync(`${verse}\n${ref}`);
-                notifier.notify({
-                    title: 'Bleezy',
-                    message: `${ref} was copied to the clipboard`
-                });  
-
-                displayVerse(verse, ref)
-            }
-        }
+function notifyUser(ref) {
+    notifier.notify({
+        title: 'Bleezy',
+        message: `${ref} was copied to the clipboard`
     });
-
-    
 }
 
 function showLoading() {
